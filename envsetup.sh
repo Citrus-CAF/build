29dd6425c03ea083635622ca15a7dfbb38899b72
Ricardo Cerqueira <github@cerqueira.org>
2011-11-25 15:30:36 +0000

    build: Preliminary support for Citrus product builds
    If building a citrus_* product, skip crawling the filesystem for
    AndroidProduct makefiles (and, of course, parsing their contents)
    and aim directly for the device/cm/<product> device configuration
    
    Change-Id: I2a5e70dda973a6fcdbba0d5e26b35b99d3f1aea2
    
    Change how we search for Citrus makefiles.
    
    build: Fix fastpath code for CITRUS_BUILD
    
     * We need to adjust to the new validations
    
    Change-Id: I9066d4f437beb9597027ee6bbb52504b5e7e84c5
    
    build: Fix citrus builds
    
    the setup variable wasn't exported, and was being missed by part
    of the scripts
    
    Change-Id: I9066d4f437beb9597027ee6bbb52504b5e7e84c5

--- envsetup.sh
+++ envsetup.sh
@@ -130,6 +130,14 @@ function check_product()
         echo "Couldn't locate the top of the tree.  Try setting TOP." >&2
         return
     fi
+
+    if (echo -n $1 | grep -q -e "^citrus_") ; then
+       CITRUS_BUILD=$(echo -n $1 | sed -e 's/^citrus_//g')
+    else
+       CITRUS_BUILD=
+    fi
+    export CITRUS_BUILD
+
         TARGET_PRODUCT=$1 \
         TARGET_BUILD_VARIANT= \
         TARGET_BUILD_TYPE= \

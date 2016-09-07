704568e23791b893b89a3e303711c03ce32477d6
Ricardo Cerqueira <ricardo@cyngn.com>
2016-09-08 00:05:21 +0100

    build_image: Unbreak verity/FEC generation
    Change-Id: Icbd36b11659ced7764599c5d2e0e91be67a3e4bc

--- tools/releasetools/build_image.py
+++ tools/releasetools/build_image.py
@@ -59,7 +59,7 @@ def RunCommand(cmd):
 
 def GetVerityFECSize(partition_size):
   cmd = "fec -s %d" % partition_size
-  status, output = commands.getstatusoutput(cmd)
+  status, output = getstatusoutput(cmd)
   if status:
     print output
     return False, 0
@@ -150,12 +150,10 @@ def AdjustPartitionSizeForVerity(partition_size, fec_supported):
 
 AdjustPartitionSizeForVerity.results = {}
 
-def BuildVerityFEC(sparse_image_path, verity_path, verity_fec_path,
-                   padding_size):
-  cmd = "fec -e -p %d %s %s %s" % (padding_size, sparse_image_path,
-                                   verity_path, verity_fec_path)
-  print cmd
-  status, output = commands.getstatusoutput(cmd)
+def BuildVerityFEC(sparse_image_path, verity_path, verity_fec_path):
+  cmd = "fec -e %s %s %s" % (sparse_image_path, verity_path, verity_fec_path)
+  print(cmd)
+  status, output = getstatusoutput(cmd)
   if status:
     print "Could not build FEC data! Error: %s" % output
     return False
@@ -225,8 +223,8 @@ def Append2Simg(sparse_image_path, unsparse_image_path, error_message):
 
 def Append(target, file_to_append, error_message):
   cmd = 'cat %s >> %s' % (file_to_append, target)
-  print cmd
-  status, output = commands.getstatusoutput(cmd)
+  print(cmd)
+  status, output = getstatusoutput(cmd)
   if status:
     print "%s: %s" % (error_message, output)
     return False

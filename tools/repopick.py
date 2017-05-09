LineageOS/android_build

repopick: try to use remote's default revision
* If the project doesn't have a revision specified, try
  to see if it's remote has one specified. If not, fall back
  the the main default revision.

Change-Id: I05129413b154e5d08d0fa9ef0ce853631b7e1562

tools/repopick.py
@@ -209,6 +209,7 @@ if __name__ == '__main__':
     manifest = subprocess.check_output(['repo', 'manifest'])
     xml_root = ElementTree.fromstring(manifest)
     projects = xml_root.findall('project')
+    remotes = xml_root.findall('remote')
     default_revision = xml_root.findall('default')[0].get('revision').split('/')[-1]
 
     #dump project data into the a list of dicts with the following data:
@@ -219,7 +220,11 @@ if __name__ == '__main__':
         path = project.get('path')
         revision = project.get('revision')
         if revision is None:
-            revision = default_revision
+            for r in remotes:
+                if r.get('name') == project.get('remote'):
+                    revision = r.get('revision')
+            if revision is None:
+                revision = default_revision
 
         if not name in project_name_to_data:
             project_name_to_data[name] = {}

Citrus-CAF/build

  to see if it's remote has one specified before falling back
  to the global default.

tools/repopick.py
@@ -206,6 +206,7 @@ if __name__ == '__main__':
     manifest = subprocess.check_output(['repo', 'manifest'])
     xml_root = ElementTree.fromstring(manifest)
     projects = xml_root.findall('project')
+    remotes = xml_root.findall('remote')
     default_revision = xml_root.findall('default')[0].get('revision').split('/')[-1]
 
     #dump project data into the a list of dicts with the following data:
@@ -216,7 +217,11 @@ if __name__ == '__main__':
         path = project.get('path')
         revision = project.get('revision')
         if revision is None:
-            revision = default_revision
+            for remote in remotes:
+                if remote.get('name') == project.get('remote'):
+                    revision = remote.get('revision')
+            if revision is None:
+                revision = default_revision
 
         if not name in project_name_to_data:
             project_name_to_data[name] = {}